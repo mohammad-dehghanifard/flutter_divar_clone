276ed18b5b220ffcd1ddda5019f75b52025cf78f
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-20 08:42:38 -0800

    upload project

---
+++ android/app/src/main/kotlin/com/dehghanifard/flutter_divar_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dehghanifard.flutter_divar_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_divar_clone/modules/auth/pages/splash_page.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Divar Clone',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
