070c6b3cd1bec0d7fb81efca2479972c4f789851
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-20 21:15:56 -0800

    completed splash page ui

--- lib/helpers/theme/theme_manager.dart
+++ lib/helpers/theme/theme_manager.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 
 class ThemeManager {
   ThemeManager._();
-  
+
   static ThemeData lightMode = ThemeData(
+    fontFamily: 'yekanBakh',
     colorScheme: const ColorScheme.light(
       primary: Color(0xFFD91313),
-      background: Color(0xFFF5F5F5)
+      background: Color(0xFFF5F5F5),
+      onPrimary: Color(0xFFFFFFFF)
     ),
     useMaterial3: true,
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle()
+    )
   );
 }
--- lib/modules/auth/pages/splash_page.dart
+++ lib/modules/auth/pages/splash_page.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          width: MediaQuery.sizeOf(context).width,
+          height: MediaQuery.sizeOf(context).height,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              // app name and loading
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // app name
+                  Text("اپلیکیشن آگهی هیتال", style: TextStyle(
+                      fontWeight: FontWeight.w700, fontSize: 26, color: Theme
+                      .of(context)
+                      .colorScheme
+                      .primary)),
+                  const SizedBox(height: 12),
+                  // loading
+                  SpinKitThreeBounce(
+                    color: Theme.of(context).colorScheme.primary,
+                    size: 20,
+                  ),
+                ],
+              ),
+              // version
+              const Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Text("ورژن : 1.0.0",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 14)))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
