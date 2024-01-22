7ae0ac5269d89ebfbe430c70d730f897173a1e48
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-21 21:19:44 -0800

    impl AuthRepository class

---
+++ lib/backend/repository/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_divar_clone/backend/repository/base_repository.dart';
+import 'package:flutter_divar_clone/backend/response/province_response.dart';
+
+class AuthRepository extends BaseRepository {
+  Future<ProvinceResponse?> getAllProvinceAndCityApi() async {
+    try{
+      final response = await dio.get("/provinces");
+      print(response.data);
+      return ProvinceResponse.fromJson(response.data);
+    }catch(e){
+      print(e);
+    }
+
+  }
+}
