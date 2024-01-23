11104a145e69cb6019abdda3e8202937da610793
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-22 23:21:05 -0800

    impl AdsRepository class

---
+++ lib/backend/repository/ads_respository.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_divar_clone/backend/repository/base_repository.dart';
+import 'package:flutter_divar_clone/backend/response/ads_response.dart';
+
+class AdsRepository extends BaseRepository {
+  Future<AdsResponse> getAndFilterAdsApi({int? cityId, int? provinceId, int? categoryId, String? orderBy, String? orderType, String? searchKeyWord}) async {
+    final Map<String,dynamic> params = {};
+    final response = await dio.get("/ad",queryParameters: params);
+    validateResponse(response);
+    print(response);
+    return AdsResponse.fromJson(response.data);
+  }
+}
